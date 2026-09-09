55ea38848b2c212acfa8e945fe04d917f2144361
Nischita-Edigar <106088398+Nischita-Edigar@users.noreply.github.com>
2026-09-09 12:33:56 +0530

    Update index.js

--- voice-frontend/src/index.js
+++ voice-frontend/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/voice">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
